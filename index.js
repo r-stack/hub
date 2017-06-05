0d0d954ec9f0d0b159ccf2fa7aa13cf4f7a1f779
Hiroshi Akiyama <hiroshi.akiyama.tf@hitachi-systems.com>
2017-06-05 18:08:15 +0900

    複数再生対応

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
-var SOUND_URL = '/sound/n127.mp3';
-var SOUND_URL2 = '/sound/Moonlight.mp3';
+//var SOUND_URL = '/sound/n127.mp3';
+//var SOUND_URL2 = '/sound/Moonlight.mp3';
 
 var SupportedAudioContext;
 try {
@@ -13,42 +13,77 @@ var context = new SupportedAudioContext();
 
 
 var source;
+var ctrls;
+
 function play(targets){
     if(!targets || targets.length === 0){
         return;
     }
     
-    var url = '/sound/' + targets[0] + '.mp3';
+    ctrls = new Array();
     
-    var buffer;
-    var request = new XMLHttpRequest();
-    request.open('GET', url, true);
-    request.responseType = 'arraybuffer'; // ArrayBufferとしてロード
-    request.send();
+    var dffs = new Array();
     
-    var d = new $.Deferred;
-    request.onload = function () {
-        // contextにArrayBufferを渡し、decodeさせる
-        context.decodeAudioData(request.response, function (buf) {
-            buffer = buf;
-            d.resolve();
+    $.each(targets, function(i, target){
+       
+        var url = '/sound/' + target + '.mp3';
+
+        
+        var request = new XMLHttpRequest();
+        request.open('GET', url, true);
+        request.responseType = 'arraybuffer'; // ArrayBufferとしてロード
+        request.send();
+        
+        
+        var d = new $.Deferred;
+        request.onload = function () {
+            // contextにArrayBufferを渡し、decodeさせる
+            context.decodeAudioData(request.response, function (buf) {
+//                buffer = buf;
+                ctrls.push(createSource(buf));
+                d.resolve();
+            });
+        };
+        
+        dffs.push(d);
+    });
+        
+    
+    $.when.apply($, dffs).then(function(){
+        
+        $.each(ctrls, function(i, ctrl){
+            
+            ctrl.source.connect(context.destination);
+            ctrl.source.start(0);
         });
-    };
-    
-    d.promise().done(function(){
-        source = context.createBufferSource();
-        source.buffer = buffer;
-        source.connect(context.destination);
-        
-        source.start(0);
-        
     });
     
-}
+    
 
+};
+
+
+function createSource(buffer) {
+    var source = context.createBufferSource();
+    var gainNode = context.createGain ? context.createGain() : context.createGainNode();
+    source.buffer = buffer;
+    // Turn on looping
+    source.loop = true;
+    // Connect source to gain.
+    source.connect(gainNode);
+    // Connect gain to destination.
+    gainNode.connect(context.destination);
+
+    return {
+      source: source,
+      gainNode: gainNode
+    };
+  }
 
 function stop(){
-    source.stop();
+    $.each(ctrls, function(i, ctrl){
+        ctrl.source.stop();
+    });
 }
 
 
